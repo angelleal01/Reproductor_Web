55502f705a4a0e684a31f801c19c5e3d1b1d4fb0
angelleal01 <angeldavid141101@gmail.com>
2024-06-19 11:04:55 -0400

    Tengo que resolver

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-const Closet = 2024
-// Para guardar constantes o variables se utiliza const 
+// Para guardar constantes o variables se utiliza const
 
-console.log(Closet)
+const tipoNumber = 2024 //numeros
+
+const tipoString = 'Quiero Dormir' //Cadena de texto
+
+const tipoBoolean = false // Verdadero o falso
+
+const tipoUndefined = undefined  // Sinonimo de Error si sale undefined
+const tipoNull = null  // Ves esta vacio
+
+const tipoArray = [
+    'arroz',
+    'carne',
+    'pollo',
+    'pasta',
+    'salsa de tomate'
+]
+
+const quecompre = ['Harina']
+
+console.log (tipoArray.concat(quecompre))
+
+const tipoObjeto = {  // Tipo objeto
+    raza: 'Kacri',
+    edad: 15,
+    dueño: 'Carlos',
+    hambre: false
+}
+
+console.log(tipoNumber)
+
+
+const user = {
+    email: 'example@leo.com',
+    password: '1234ñ',
+    name: 'Angel Leal',
+    username: 'angelleal01',
+    address: {
+        city: 'Valencia',
+        state: 'Carabobo',
+        country: 'Venezuela'
+    }
+}
+
+console.log(user.address.state)
+
+// Operadores Logicos y Matematicos
+
