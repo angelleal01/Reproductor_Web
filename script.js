25b0c6de529dd8190f4f8c5e6a4612ff264b3af9
angelleal01 <angeldavid141101@gmail.com>
2024-06-19 12:07:22 -0400

    Tengo que modificar cosas

--- script.js
+++ script.js
@@ -1,49 +1,71 @@
-// Para guardar constantes o variables se utiliza const
+// // Para guardar constantes o variables se utiliza const
 
-const tipoNumber = 2024 //numeros
+// const tipoNumber = 2024 //numeros
 
-const tipoString = 'Quiero Dormir' //Cadena de texto
+// const tipoString = 'Quiero Dormir' //Cadena de texto
 
-const tipoBoolean = false // Verdadero o falso
+// const tipoBoolean = false // Verdadero o falso
 
-const tipoUndefined = undefined  // Sinonimo de Error si sale undefined
-const tipoNull = null  // Ves esta vacio
+// const tipoUndefined = undefined  // Sinonimo de Error si sale undefined
+// const tipoNull = null  // Ves esta vacio
 
-const tipoArray = [
-    'arroz',
-    'carne',
-    'pollo',
-    'pasta',
-    'salsa de tomate'
-]
+// const tipoArray = [
+//     'arroz',
+//     'carne',
+//     'pollo',
+//     'pasta',
+//     'salsa de tomate'
+// ]
 
-const quecompre = ['Harina']
+// const quecompre = ['Harina']
 
-console.log (tipoArray.concat(quecompre))
+// console.log (tipoArray.concat(quecompre))
 
-const tipoObjeto = {  // Tipo objeto
-    raza: 'Kacri',
-    edad: 15,
-    dueño: 'Carlos',
-    hambre: false
-}
+// const tipoObjeto = {  // Tipo objeto
+//     raza: 'Kacri',
+//     edad: 15,
+//     dueño: 'Carlos',
+//     hambre: false
+// }
 
-console.log(tipoNumber)
+// console.log(tipoNumber)
 
 
-const user = {
-    email: 'example@leo.com',
-    password: '1234ñ',
-    name: 'Angel Leal',
-    username: 'angelleal01',
-    address: {
-        city: 'Valencia',
-        state: 'Carabobo',
-        country: 'Venezuela'
-    }
-}
+// const user = {
+//     email: 'example@leo.com',
+//     password: '1234ñ',
+//     name: 'Angel Leal',
+//     username: 'angelleal01',
+//     address: {
+//         city: 'Valencia',
+//         state: 'Carabobo',
+//         country: 'Venezuela'
+//     }
+// }
 
-console.log(user.address.state)
+// console.log(user.address.state)
 
-// Operadores Logicos y Matematicos
+// // Operadores Logicos y Matematicos
 
+// //las funciones son bloques de procedimientos
+
+
+// const funcion = () => {  //formula para una funcion tipo flecha
+//     //El codigo va aqui
+// }
+
+axios.get('https://leonardoapi.onrender.com/songs').then((res) => {
+
+    //Guardo la lista de canciones
+    const songlist = res.data.songs
+
+    console.log(songlist)
+
+    //Recorriendo la lista de Canciones
+    songlist.map((song) => {
+
+        console.log(song)
+
+    })
+
+})
