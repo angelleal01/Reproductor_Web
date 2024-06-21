edbe35c49eddc8432b250a6e20f36d48381cc67a
angelleal01 <angeldavid141101@gmail.com>
2024-06-21 17:03:28 -0400

    Casi listo

--- script.js
+++ script.js
@@ -59,10 +59,35 @@ axios.get('https://leonardoapi.onrender.com/songs').then((res) => {
     //Guardo la lista de canciones
     const songlist = res.data.songs
 
+    const tracklist = document.getElementById('track-list')
+
     console.log(songlist)
 
     //Recorriendo la lista de Canciones
     songlist.map((song) => {
+        //Por cada cancion
+
+        const div = document.createElement('div')
+        div.classList.add('tarjetas__Recomendadas')
+
+        div.innerHTML = `
+            <img src="${song.path.front}" alt="">
+
+                <div class="tarjetas__Recomendadas__abajo">
+
+                    <div class="tarjetas__Recomendadas__abajo__info">
+                        <p>${song.title}</p>
+                        <p>${song.author}</p>
+                    </div>
+
+                    <img src="/assets/icono_chiquito_playlist.svg" alt="">
+
+                </div>
+        
+        `
+
+        tracklist.appendChild(div)
+
 
         console.log(song)
 
